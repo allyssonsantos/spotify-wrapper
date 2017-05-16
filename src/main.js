a0555799de4f1e82112a0c6260f1d03555294cd2
Allysson dos Santos <aallysson0@gmail.com>
2017-05-16 20:01:15 -0300

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,10 @@
+const FizzBuzz = (num) => {
+  if (num === 0) return num;
+  if (num % 3 === 0 && num % 5 === 0) return 'FizzBuzz';
+  if (num % 3 === 0) return 'Fizz';
+  if (num % 5 === 0) return 'Buzz';
+
+  return num;
+};
+
+export default FizzBuzz;
